107412b8535f9e3b75116984ba9eac25b6d241cd
Ron Asnahon <asnahonron@gmail.com>
2025-06-28 22:34:27 +0800

    Add files via upload

---
+++ rust/distance/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod distance_avx;
+pub mod distance_neon;
+pub mod distance_sse;
+pub mod types;
+
+#[cfg(all(target_feature = "avx", target_feature = "fma"))]
+pub use distance_avx::*;
+
+#[cfg(target_feature = "neon")]
+pub use distance_neon::*;
+
+#[cfg(target_feature = "sse")]
+pub use distance_sse::*;
+pub use types::*;
+
+pub fn normalize(vector: &[f32]) -> Vec<f32> {
+    let norm = vector.iter().map(|x| x * x).sum::<f32>().sqrt();
+    vector.iter().map(|x| x / (norm + 1e-32)).collect()
+}
